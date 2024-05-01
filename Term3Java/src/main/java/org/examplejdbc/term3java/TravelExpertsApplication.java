df29a41ddbbf41edd559f692e4923de3981ef817
nnedimma-orji <128959660+nnedimma-orji@users.noreply.github.com>
2024-05-01 13:56:46 -0600

    uuuuuUUUUUAAAAAAAAAAAAAAAAAAAAHGHGHGHGHG

---
+++ Term3Java/src/main/java/org/examplejdbc/term3java/TravelExpertsApplication.java
@@ -0,0 +1,27 @@
+package org.examplejdbc.term3java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class TravelExpertsApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/org/examplejdbc/term3java/MainView.fxml"));
+        BorderPane root = mainLoader.load();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Travel Experts Application");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
