f2b8efddf96041223c38b335c85710ed50606aa8
nnedimma-orji <128959660+nnedimma-orji@users.noreply.github.com>
2024-05-01 13:56:46 -0600

    uuuuuUUUUUAAAAAAAAAAAAAAAAAAAAHGHGHGHGHG

---
+++ Term3Java/src/main/java/org/examplejdbc/term3java/AgentDialogueController.java
@@ -0,0 +1,221 @@
+/**
+ * Sample Skeleton for 'agentdialogue-view.fxml' Controller Class
+ */
+
+package org.examplejdbc.term3java;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AgentDialogueController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="tfAgentId"
+    private TextField tfAgentId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblMode"
+    private Label lblmode; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtFirstName"
+    private TextField tfAgtFirstName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtMiddleInitial"
+    private TextField tfAgtMiddleInitial; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtLastName"
+    private TextField tfAgtLastName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtBusPhone"
+    private TextField tfAgtBusPhone; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtEmail"
+    private TextField tfAgtEmail; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgtPosition"
+    private TextField tfAgtPosition; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfAgencyId"
+    private TextField tfAgencyId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSave"
+    private Button btnSave; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnDelete"
+    private Button btnDelete; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCancel"
+    private Button btnCancel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblMode"
+    private Label lblMode; // Value injected by FXMLLoader
+
+    private String mode;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert tfAgentId != null : "fx:id=\"tfAgentId\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgtFirstName != null : "fx:id=\"tfAgtFirstName\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgtMiddleInitial != null : "fx:id=\"tfAgtMiddleInitial\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert lblmode != null : "fx:id=\"lblmode\" was not injected: check your FXML file 'dialog-view.fxml'.";
+        assert tfAgtLastName != null : "fx:id=\"tfAgtLastName\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgtBusPhone != null : "fx:id=\"tfAgtBusPhone\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgtEmail != null : "fx:id=\"tfAgtEmail\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgtPosition != null : "fx:id=\"tfAgtPosition\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert tfAgencyId != null : "fx:id=\"tfAgencyId\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert btnSave != null : "fx:id=\"btnSave\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+        assert lblMode != null : "fx:id=\"lblMode\" was not injected: check your FXML file 'agentdialogue-view.fxml'.";
+
+        tfAgentId.setDisable(true);
+        btnSave.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btnSaveClicked(mouseEvent);
+            }
+        });
+
+        btnDelete.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btnDeleteClicked(mouseEvent);
+            }
+        });
+
+        btnCancel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Node node = (Node) mouseEvent.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                stage.close();
+            }
+        });
+    }
+
+    private void btnDeleteClicked(MouseEvent mouseEvent) {
+        Properties p = getProperties();
+        try {
+            Connection connection = DriverManager.getConnection(p.getProperty("url"), p);
+            String sql = "delete from agents where AgentId=?";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, Integer.parseInt(tfAgentId.getText()));
+            int numRows = stmt.executeUpdate();
+            if (numRows==0) {
+                System.out.println("delete failed");
+            } else {
+                System.out.println("delete successful");
+            }
+            connection.close();
+            Node node = (Node) mouseEvent.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
+        } catch (SQLIntegrityConstraintViolationException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Delete failed");
+            alert.setContentText("Agent has customers and cannot be deleted");
+            alert.showAndWait();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    private void btnSaveClicked(MouseEvent mouseEvent) {
+        Properties p = getProperties();
+        try {
+            Connection connection = DriverManager.getConnection(p.getProperty("url"), p);
+            String sql = "";
+            if (mode.equals("edit"))
+            {
+                //build update sql
+                sql = "UPDATE `agents` SET `AgtFirstName`=?," +
+                        "`AgtMiddleInitial`=?,`AgtLastName`=?,`AgtBusPhone`=?," +
+                        "`AgtEmail`=?,`AgtPosition`=?,`AgencyId`=? WHERE AgentId=?";
+            }
+            else
+            {
+                //build insert sql
+                sql = "INSERT INTO `agents`(`AgtFirstName`, `AgtMiddleInitial`, " +
+                        "`AgtLastName`, `AgtBusPhone`, `AgtEmail`, `AgtPosition`," +
+                        " `AgencyId`) VALUES (?,?,?,?,?,?,?)";
+            }
+             //updating the database with the agent info
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, tfAgtFirstName.getText());
+            stmt.setString(2, tfAgtMiddleInitial.getText());
+            stmt.setString(3, tfAgtLastName.getText());
+            stmt.setString(4, tfAgtBusPhone.getText());
+            stmt.setString(5, tfAgtEmail.getText());
+            stmt.setString(6, tfAgtPosition.getText());
+            stmt.setInt(7, Integer.parseInt(tfAgencyId.getText()));
+            if (mode.equals("edit"))
+            {
+                stmt.setInt(8, Integer.parseInt(tfAgentId.getText()));
+            }
+            int numRows = stmt.executeUpdate();
+            if (numRows==0) {
+                System.out.println("update failed");
+            } else {
+                System.out.println("update successful");
+            }
+            connection.close();
+            Node node = (Node) mouseEvent.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void processAgent(Agent t1) {
+
+        tfAgentId.setText(t1.getAgentId() + "");
+        tfAgtFirstName.setText(t1.getAgtFirstName());
+        tfAgtMiddleInitial.setText(t1.getAgtMiddleInitial());
+        tfAgtLastName.setText(t1.getAgtLastName());
+        tfAgtBusPhone.setText(t1.getAgtBusPhone());
+        tfAgtEmail.setText(t1.getAgtEmail());
+        tfAgtPosition.setText(t1.getAgtPosition());
+        tfAgencyId.setText(t1.getAgencyId() + "");
+    }
+
+    private Properties getProperties() {
+        try {
+            FileInputStream fis = new FileInputStream("c:\\connection.properties");
+            Properties p = new Properties();
+            p.load(fis);
+            return p;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void passMode(String mode) {
+        this.mode = mode;
+        lblmode.setText(mode);
+
+        if (mode.equals("add"))
+        {
+            btnDelete.setVisible(false);
+        }
+    }
+}
---
+++ Term3Java/src/main/java/org/examplejdbc/term3java/TravelExpertsController.java
@@ -0,0 +1,138 @@
+/**
+ * Sample Skeleton for 'travelExperts-view.fxml' Controller Class
+ */
+
+package org.examplejdbc.term3java;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class TravelExpertsController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+    @FXML // fx:id="colAgentId"
+    private TableColumn<Customer, Integer> colAgentId; // Value injected by FXMLLoader
+
+
+    @FXML // fx:id="colCustAddress"
+    private TableColumn<Customer, String> colCustAddress; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustBusPhone"
+    private TableColumn<Customer, String> colCustBusPhone; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustCity"
+    private TableColumn<Customer, String> colCustCity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustCountry"
+    private TableColumn<Customer, String> colCustCountry; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustEmail"
+    private TableColumn<Customer, String> colCustEmail; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustFirstName"
+    private TableColumn<Customer, String> colCustFirstName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustHomePhone"
+    private TableColumn<Customer, String> colCustHomePhone; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustLastName"
+    private TableColumn<Customer, String> colCustLastName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustPostal"
+    private TableColumn<Customer, String> colCustPostal; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustomerId"
+    private TableColumn<Customer, Integer> colCustomerId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colProv"
+    private TableColumn<Customer, String> colCustProv; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tpCustomer"
+    private Tab tpCustomer; // Value injected by FXMLLoader
+    @FXML // fx:id="tvCustomer"
+    private TableView<Customer> tvCustomer; // Value injected by FXMLLoader
+
+    public ObservableList<Customer> customerList = FXCollections.observableArrayList();
+    private
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert colAgentId != null : "fx:id=\"colAgentId\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustAddress != null : "fx:id=\"colCustAddress\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustBusPhone != null : "fx:id=\"colCustBusPhone\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustCity != null : "fx:id=\"colCustCity\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustCountry != null : "fx:id=\"colCustCountry\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustEmail != null : "fx:id=\"colCustEmail\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustFirstName != null : "fx:id=\"colCustFirstName\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustHomePhone != null : "fx:id=\"colCustHomePhone\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustLastName != null : "fx:id=\"colCustLastName\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustPostal != null : "fx:id=\"colCustPostal\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustomerId != null : "fx:id=\"colCustomerId\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert colCustProv != null : "fx:id=\"colProvince\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert tpCustomer != null : "fx:id=\"tpCustomer\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+        assert tvCustomer != null :  "fx:id=\"tvCustomer\" was not injected: check your FXML file 'travelExperts-view.fxml'.";
+
+
+        getCustomerInfo();
+        colCustomerId.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("customerId"));
+        colCustFirstName.setCellValueFactory(new PropertyValueFactory<Customer, String>("custFirstName"));
+        colCustLastName.setCellValueFactory(new PropertyValueFactory<Customer, String>("custLastName"));
+        colCustAddress.setCellValueFactory(new PropertyValueFactory<Customer, String>("custAddress"));
+        colCustCity.setCellValueFactory(new PropertyValueFactory<Customer, String>("custCity"));
+        colCustProv.setCellValueFactory(new PropertyValueFactory<Customer, String>("custProv"));
+        colCustPostal.setCellValueFactory(new PropertyValueFactory<Customer, String>("custPostal"));
+        colCustCountry.setCellValueFactory(new PropertyValueFactory<Customer, String>("custCountry"));
+        colCustHomePhone.setCellValueFactory(new PropertyValueFactory<Customer, String>("custHomePhone"));
+        colCustBusPhone.setCellValueFactory(new PropertyValueFactory<Customer, String>("custBusPhone"));
+        colCustEmail.setCellValueFactory(new PropertyValueFactory<Customer, String>("custEmail"));
+        colAgentId.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("agentId"));
+        tvCustomer.setItems(customerList);
+    }
+
+    private void getCustomerInfo() {
+        String url = "";
+        String user = "";
+        String password = "";
+
+        try {
+            FileInputStream fis = new FileInputStream("D:\\connection.properties");
+            Properties prop = new Properties();
+            prop.load(fis);
+            url = (String) prop.get("url");
+            user = (String) prop.get("user");
+            password = (String) prop.get("password");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from customers");
+            while (rs.next())
+            {
+                customerList.add(new Customer(rs.getInt(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getString(8),
+                        rs.getString(9), rs.getString(10), rs.getString(11), rs.getInt(12)));
+            }
+            conn.close();
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
